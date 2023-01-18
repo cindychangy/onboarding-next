730e5012ccec2ffbe49c11448516c5d079418ba1
Cindy Chang <cindyisachang@gmail.com>
2023-01-18 11:41:41 +0100

    config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,7 @@
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'next/core-web-vitals',
-  ],
+  extends: ['prettier', 'next/core-web-vitals'],
   plugins: ['prettier'],
   rules: {
-    // In an ideal world, we'd never have to use @ts-ignore, but that's not
-    // possible right now.
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-
-    // Again, in theory this is a good rule, but it can cause a bit of
-    // unhelpful noise.
-    '@typescript-eslint/explicit-function-return-type': 'off',
-
-    // Another theoretically good rule, but sometimes we know better than
-    // the linter.
-    '@typescript-eslint/no-non-null-assertion': 'off',
-
     // Accessibility is important to EUI. Enforce all a11y rules.
     'jsx-a11y/accessible-emoji': 'error',
     'jsx-a11y/alt-text': 'error',
